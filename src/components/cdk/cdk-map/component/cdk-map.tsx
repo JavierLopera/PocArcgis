519b6ed250964fe3ccdb0473d2eff8416d56917d
Javi <100303553@alumnos.uc3m.es>
2020-04-06 13:35:54 +0200

    Añadidos tipos de mapas

--- src/components/cdk/cdk-map/component/cdk-map.tsx
+++ src/components/cdk/cdk-map/component/cdk-map.tsx
@@ -39,17 +39,22 @@ export class CDKMap {
 	esriMap: __esri.Map; // Instancia del mapa
 	esriMapView: __esri.MapView; // Instancia de la vista del mapa
 	municipalitiesFeatureLayer: __esri.FeatureLayer; // Caracteristicas del mapa
+	basemapToggle; // Permite alterna entre el modod satelite y Topografico
+	basemapGallery; // Permite cambiar mapas base de un grupo de Arcgis Online
 
 	constructor() {
 		loadCss(`${this.esriMapOptions.url}/esri/css/main.css`);
 
 		loadModules(
-			["esri/Map", "esri/layers/FeatureLayer"],
+			[
+				"esri/Map",
+				"esri/layers/FeatureLayer"
+			],
 			this.esriMapOptions
 		).then(
 			([EsriMap, FeatureLayer]: [
 				__esri.MapConstructor,
-				__esri.FeatureLayerConstructor
+				__esri.FeatureLayerConstructor,
 			]) => {
 				this.esriMap = new EsriMap({
 					basemap: this.viewTypeMap
@@ -71,95 +76,54 @@ export class CDKMap {
 
 	componentDidLoad() {
 		this.createEsriMapView()
-			// .then(() => this.zoomToUrlObjectId())
-			// .then(() => this.addZoomOnClickAndUrlUpdate());
+		// .then(() => this.zoomToUrlObjectId())
+		// .then(() => this.addZoomOnClickAndUrlUpdate());
 	}
 
 	/**
 	* Inicializa la vista del mapa
 	*/
 	createEsriMapView() {
-		return loadModules(["esri/views/MapView"], this.esriMapOptions).then(
-			([EsriMapView]: [__esri.MapViewConstructor]) => {
-				const mapDiv = this.rippleLinkElement;
+		return loadModules([
+			"esri/views/MapView",
+			"esri/widgets/BasemapToggle",
+			"esri/widgets/BasemapGallery"], this.esriMapOptions).then(
+				([EsriMapView, BasemapToggle, BasemapGallery]:
+					[
+						__esri.MapViewConstructor,
+						__esri.BasemapToggleConstructor,
+						__esri.BasemapGalleryConstructor
+					]) => {
 
-				this.esriMapView = new EsriMapView({
-					container: mapDiv,
-					zoom: this.viewZoomMap,
-					center: this.viewPositionMap,
-					map: this.esriMap
-				});
-			}
-		);
+					const mapDiv = this.rippleLinkElement;
+
+					this.esriMapView = new EsriMapView({
+						container: mapDiv,
+						zoom: this.viewZoomMap,
+						center: this.viewPositionMap,
+						map: this.esriMap
+					});
+
+					this.basemapToggle = new BasemapToggle({
+						view: this.esriMapView,
+						nextBasemap: "satellite"
+					});
+
+					this.basemapGallery = new BasemapGallery({
+						view: this.esriMapView,
+						source: {
+							portal: {
+								url: "https://www.arcgis.com",
+								useVectorBasemaps: false  // Load vector tile basemaps
+							}
+						}
+					});
+
+					this.esriMapView.ui.add(this.basemapToggle, "bottom-right");
+					this.esriMapView.ui.add(this.basemapGallery, "top-right");
+				}
+			);
 	}
-
-	// Metodo encargado de ir a una ubicación concreta
-	// zoomToUrlObjectId(duration = 1600) {
-	// 	debugger
-	// 	if (this.match && this.match.params && this.match.params.objectid) {
-	// 		debugger
-	// 		this.municipalitiesFeatureLayer
-	// 			.queryFeatures({
-	// 				where: "objectid = " + this.match.params.objectid,
-	// 				num: 1,
-	// 				returnGeometry: true
-	// 			})
-	// 			.then(results => {
-	// 				if (results.features.length) {
-	// 					const firstResult = results.features[0];
-	// 					this.zoomToAndHighlighFeature(firstResult, duration);
-	// 				}
-	// 			});
-	// 	}
-	// }
-
-	// zoomToAndHighlighFeature(feature: __esri.Graphic, duration = 1600) {
-	// 	debugger
-	// 	this.esriMapView.when(() => {
-	// 		const symbol = {
-	// 			type: "simple-fill",
-	// 			color: [51, 51, 204, 0.9],
-	// 			style: "solid",
-	// 			outline: {
-	// 				color: "white",
-	// 				width: 1
-	// 			}
-	// 		};
-
-	// 		const highlightPolygon = feature.clone();
-	// 		highlightPolygon.set("symbol", symbol);
-
-	// 		this.esriMapView.graphics.removeAll();
-	// 		this.esriMapView.graphics.add(highlightPolygon);
-
-	// 		this.esriMapView.goTo(feature.geometry, {
-	// 			duration: duration,
-	// 			easing: "ease-in"
-	// 		});
-	// 	});
-	// }
-
-	// Metodo encargado de que cuando haces click te redirija a el lugar correspondiente, No funciona
-	// addZoomOnClickAndUrlUpdate() {
-	// 	debugger
-	// 	this.esriMapView.on("click", evt => {
-	// 		this.esriMapView
-	// 			.whenLayerView(this.municipalitiesFeatureLayer)
-	// 			.then((lyrView: __esri.FeatureLayerView) => {
-	// 				lyrView.queryFeatures().then(results => {
-	// 					results.features.some(f => {
-	// 						const polygon = f.geometry as __esri.Polygon;
-	// 						if (polygon.contains(evt.mapPoint)) {
-	// 							this.history.push(`/map/${f.attributes.ObjectId}`, {});
-	// 							this.zoomToAndHighlighFeature(f, 500);
-	// 							return true;
-	// 						}
-	// 						return false;
-	// 					});
-	// 				});
-	// 			});
-	// 	});
-	// }
 
 	render() {
 
@@ -173,38 +137,22 @@ export class CDKMap {
 }
 
 
-// connectedCallback() {
-// 	// lazy load the required ArcGIS API for JavaScript modules and CSS
-// 	loadModules(['esri/Map', 'esri/views/MapView'], { css: true })
-// 		.then(([ArcGISMap, MapView]) => {
-// 			const map = new ArcGISMap({
-// 				basemap: 'topo-vector'
-// 			});
+// // puntos
+// const trailheadsLayer = new FeatureLayer({
+// 	url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
+// });
 
-// 			this.view = new MapView('webmap', {
-// 				container: this.rippleLinkElement.current,
-// 				map: map,
-// 				center: [-118, 34],
-// 				zoom: 8
-// 			});
-// 		});
-// }
+// // Lineas
+// const trailsLayer = new FeatureLayer({
+// 	url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0"
+// });
 
-// disconnectedCallback() {
-// 	if (this.view) {
-// 		// destroy the map view
-// 		this.view.container = null;
-// 	}
-// }
+// // poligonos
+// const parksLayer = new FeatureLayer({
+// 	url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space/FeatureServer/0"
+// });
 
-
-// /** Ciclo de vida ejecutado tras el primer Render después de la carga */
-// componentDidLoad() { }
-
-// /** Ciclo de vida ejecutado tras actualizarse */
-// componentDidUpdate() { }
-
-// /** Ciclo de vida al eliminar la instancia del componente */
-// componentDidUnload() { }
-
-{/* <div id="webmap" class="webmap" ref={el => this.rippleLinkElement = el as HTMLDivElement} /> */ }
+// this.esriMap.add(this.municipalitiesFeatureLayer);
+// this.esriMap.add(trailheadsLayer);
+// this.esriMap.add(trailsLayer, 0);
+// this.esriMap.add(parksLayer, 0);
