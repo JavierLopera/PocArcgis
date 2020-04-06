3d56766b07350ee6ec742c90ca3a09e079a2e364
Javi <100303553@alumnos.uc3m.es>
2020-04-06 15:30:03 +0200

    Añadido formas, no funciona correctamente

--- src/components/cdk/cdk-map/component/cdk-map.tsx
+++ src/components/cdk/cdk-map/component/cdk-map.tsx
@@ -41,6 +41,8 @@ export class CDKMap {
 	municipalitiesFeatureLayer: __esri.FeatureLayer; // Caracteristicas del mapa
 	basemapToggle; // Permite alterna entre el modod satelite y Topografico
 	basemapGallery; // Permite cambiar mapas base de un grupo de Arcgis Online
+	graphicsLayer;
+	sketch;
 
 	constructor() {
 		loadCss(`${this.esriMapOptions.url}/esri/css/main.css`);
@@ -48,16 +50,21 @@ export class CDKMap {
 		loadModules(
 			[
 				"esri/Map",
-				"esri/layers/FeatureLayer"
+				"esri/layers/FeatureLayer",
+				"esri/layers/GraphicsLayer"
 			],
 			this.esriMapOptions
 		).then(
-			([EsriMap, FeatureLayer]: [
+			([EsriMap, FeatureLayer, GraphicsLayer]: [
 				__esri.MapConstructor,
 				__esri.FeatureLayerConstructor,
+				__esri.GraphicsLayerConstructor
 			]) => {
+				this.graphicsLayer = new GraphicsLayer();
+
 				this.esriMap = new EsriMap({
-					basemap: this.viewTypeMap
+					basemap: this.viewTypeMap,
+					layers: [this.graphicsLayer]
 				});
 
 				this.municipalitiesFeatureLayer = new FeatureLayer({
@@ -87,42 +94,73 @@ export class CDKMap {
 		return loadModules([
 			"esri/views/MapView",
 			"esri/widgets/BasemapToggle",
-			"esri/widgets/BasemapGallery"], this.esriMapOptions).then(
-				([EsriMapView, BasemapToggle, BasemapGallery]:
-					[
-						__esri.MapViewConstructor,
-						__esri.BasemapToggleConstructor,
-						__esri.BasemapGalleryConstructor
-					]) => {
+			"esri/widgets/BasemapGallery",
+			"esri/widgets/Sketch"
+		], this.esriMapOptions).then(
+			([EsriMapView, BasemapToggle, BasemapGallery, Sketch]:
+				[
+					__esri.MapViewConstructor,
+					__esri.BasemapToggleConstructor,
+					__esri.BasemapGalleryConstructor,
+					__esri.SketchConstructor
+				]) => {
 
-					const mapDiv = this.rippleLinkElement;
+				const mapDiv = this.rippleLinkElement;
 
-					this.esriMapView = new EsriMapView({
-						container: mapDiv,
-						zoom: this.viewZoomMap,
-						center: this.viewPositionMap,
-						map: this.esriMap
-					});
+				this.esriMapView = new EsriMapView({
+					container: mapDiv,
+					zoom: this.viewZoomMap,
+					center: this.viewPositionMap,
+					map: this.esriMap
+				});
 
-					this.basemapToggle = new BasemapToggle({
-						view: this.esriMapView,
-						nextBasemap: "satellite"
-					});
+				this.basemapToggle = new BasemapToggle({
+					view: this.esriMapView,
+					nextBasemap: "satellite"
+				});
 
-					this.basemapGallery = new BasemapGallery({
-						view: this.esriMapView,
-						source: {
-							portal: {
-								url: "https://www.arcgis.com",
-								useVectorBasemaps: false  // Load vector tile basemaps
-							}
+				this.basemapGallery = new BasemapGallery({
+					view: this.esriMapView,
+					source: {
+						portal: {
+							url: "https://www.arcgis.com",
+							useVectorBasemaps: false  // Load vector tile basemaps
 						}
-					});
+					}
+				});
 
-					this.esriMapView.ui.add(this.basemapToggle, "bottom-right");
-					this.esriMapView.ui.add(this.basemapGallery, "top-right");
+				this.sketch = new Sketch({
+					view: this.esriMapView,
+					layer: this.graphicsLayer
+				});
+
+				const stroke = {
+					color: [255, 0, 0],
+					width: 1
 				}
-			);
+
+				//*** White fill color with 50% transparency ***//
+				const fillColor = [255, 255, 255, .5];
+
+				//*** Override all of the default symbol colors and sizes ***//
+				const pointSymbol = this.sketch.viewModel.pointSymbol;
+				pointSymbol.color = fillColor;
+				pointSymbol.outline = stroke;
+				pointSymbol.size = 8;
+
+				const polylineSymbol = this.sketch.viewModel.polylineSymbol;
+				polylineSymbol.color = stroke.color;
+				polylineSymbol.width = stroke.width;
+
+				const polygonSymbol = this.sketch.viewModel.polygonSymbol;
+				polygonSymbol.color = fillColor;
+				polygonSymbol.outline = stroke;
+
+				this.esriMapView.ui.add(this.basemapToggle, "bottom-right");
+				this.esriMapView.ui.add(this.basemapGallery, "top-right");
+				this.esriMapView.ui.add(this.sketch, "bottom-left");
+			}
+		);
 	}
 
 	render() {
@@ -135,24 +173,3 @@ export class CDKMap {
 		);
 	}
 }
-
-
-// // puntos
-// const trailheadsLayer = new FeatureLayer({
-// 	url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
-// });
-
-// // Lineas
-// const trailsLayer = new FeatureLayer({
-// 	url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0"
-// });
-
-// // poligonos
-// const parksLayer = new FeatureLayer({
-// 	url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space/FeatureServer/0"
-// });
-
-// this.esriMap.add(this.municipalitiesFeatureLayer);
-// this.esriMap.add(trailheadsLayer);
-// this.esriMap.add(trailsLayer, 0);
-// this.esriMap.add(parksLayer, 0);
