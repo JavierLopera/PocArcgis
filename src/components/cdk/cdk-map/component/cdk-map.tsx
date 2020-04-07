5b3a1cb83be76db444cb85e40050bd9a83235e40
Javi <100303553@alumnos.uc3m.es>
2020-04-07 12:29:44 +0200

    Init prueba

--- src/components/cdk/cdk-map/component/cdk-map.tsx
+++ src/components/cdk/cdk-map/component/cdk-map.tsx
@@ -33,61 +33,6 @@ export class CDKMap {
 
 	esriMap: __esri.Map; // Instancia del mapa
 	esriMapView: __esri.MapView; // Instancia de la vista del mapa
-	graphicsLayer;
-
-	point = {
-		type: "point",
-		longitude: -3.691,
-		latitude: 40.415
-	};
-
-	symbol = {
-		type: "picture-marker",  // autocasts as new PictureMarkerSymbol()
-		url: "https://static.arcgis.com/images/Symbols/Shapes/BlackStarLargeB.png",
-		width: "40px",
-		height: "40px"
-	};
-
-	attributes = {
-		Name: "Mi ejemplo",  // The name of the
-		Location: " Ejemplo correos",  // The owner of the
-	};
-
-	// Create popup template
-	popupTemplate = {
-		title: this.attributes.Name,
-		content: `I am located at <b>${this.attributes.Location}</b>.`
-	};
-
-	simpleMarkerSymbol = {
-		type: "simple-marker",
-		color: [226, 119, 40],  // orange
-		outline: {
-			color: [255, 255, 255], // white
-			width: 1
-		}
-	};
-
-	// Vertices poligono
-	polygon = {
-		type: "polygon",
-		rings: [
-			[-3.669, 40.269],
-			[-3.664, 40.201],
-			[-3.568, 40.239],
-			[-3.583, 40.332],
-		]
-	};
-
-	// Estilo poligono
-	simpleFillSymbol = {
-		type: "simple-fill",
-		color: [227, 139, 79, 0.8],  // orange, opacity 80%
-		outline: {
-			color: [255, 255, 255],
-			width: 1
-		}
-	};
 
 	constructor() {
 		loadCss(`${this.esriMapOptions.url}/esri/css/main.css`);
@@ -95,20 +40,15 @@ export class CDKMap {
 		loadModules(
 			[
 				"esri/Map",
-				"esri/views/MapView",
-				"esri/Graphic",
-				"esri/layers/GraphicsLayer"
+				"esri/views/MapView"
 			],
 			this.esriMapOptions
 		).then(
-			([Map, MapView, Graphic, GraphicsLayer]: [
+			([Map, MapView]: [
 				__esri.MapConstructor,
-				__esri.MapViewConstructor,
-				any,
-				__esri.GraphicsLayerConstructor
+				__esri.MapViewConstructor
 			]) => {
 				const mapDiv = this.rippleLinkElement;
-				this.graphicsLayer = new GraphicsLayer();
 
 				this.esriMap = new Map({
 					basemap: this.viewTypeMap
@@ -120,103 +60,11 @@ export class CDKMap {
 					center: this.viewPositionMap,
 					map: this.esriMap
 				});
-
-				// const pointGraphic = new Graphic({
-				// 	geometry: this.point,
-				// 	symbol: this.symbol
-				// });
-
-				// Localizador
-				const coordsWidget = document.createElement("div");
-				coordsWidget.id = "coordsWidget";
-				coordsWidget.className = "esri-widget esri-component";
-				coordsWidget.style.padding = "7px 15px 5px";
-
-				const showCoordinates = (pt) => {
-					var coords = "Lat/Lon " + pt.latitude.toFixed(3) + " " + pt.longitude.toFixed(3) +
-						" | Scale 1:" + Math.round(this.esriMapView.scale * 1) / 1 +
-						" | Zoom " + this.esriMapView.zoom;
-					coordsWidget.innerHTML = coords;
-				}
-
-				this.esriMapView.watch("stationary", () => {
-					showCoordinates(this.esriMapView.center);
-				});
-
-				this.esriMapView.on("pointer-move", (evt) => {
-					showCoordinates(this.esriMapView.toMap({ x: evt.x, y: evt.y }));
-				});
-
-				//Poligonos
-				const polygonGraphic = new Graphic({
-					geometry: this.polygon,
-					symbol: this.simpleFillSymbol
-				});
-
-				// Pop up
-				const pointGraphic = new Graphic({
-					geometry: this.point,
-					symbol: this.symbol,
-					//*** ADD ***//
-					attributes: this.attributes,
-					popupTemplate: this.popupTemplate
-				});
-
-
-				// Add
-
-				this.graphicsLayer.add(pointGraphic);
-				this.graphicsLayer.add(polygonGraphic);
-				this.graphicsLayer.add(pointGraphic);
-
-				this.esriMap.add(this.graphicsLayer);
-
-				// Localizador
-				this.esriMapView.ui.add(coordsWidget, "bottom-right");
-
-				const pictureGraphic = new Graphic({
-					geometry: {
-						type: "point",
-						longitude: -3.491,
-						latitude: 40.480
-					},
-					symbol: {
-						type: "picture-marker",
-						url: "https://developers.arcgis.com/labs/images/bluepin.png",
-						width: "14px",
-						height: "26px"
-					}
-				});
-
-				const textGraphic = new Graphic({
-					geometry: {
-					  type: "point",
-					  longitude: -3.291,
-					  latitude: 40.480
-					},
-					symbol: {
-					   type: "text",
-					   color: [25,25,25],
-					   haloColor: [255,255,255],
-					   haloSize: "1px",
-					   text: "This is my location!",
-					   xoffset: 0,
-					   yoffset: -25,
-					   font: {
-						 size: 12
-					  }
-					}
-				  });
-			 
-				  this.graphicsLayer.add(textGraphic);
-
-				this.graphicsLayer.add(pictureGraphic);
 			}
 		);
 	}
 
 	render() {
-
 
 		return (
 			<Host>
