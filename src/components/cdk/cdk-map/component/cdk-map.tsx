f59d0b2ceb043952aaa6c67b01e6accf2c2cbc4d
Javi <100303553@alumnos.uc3m.es>
2020-04-07 15:52:11 +0200

    save examples

--- src/components/cdk/cdk-map/component/cdk-map.tsx
+++ src/components/cdk/cdk-map/component/cdk-map.tsx
@@ -19,7 +19,7 @@ export class CDKMap {
 	@Prop() viewTypeMap = 'streets-navigation-vector';
 
 	/* Zoom por defecto de la vista del mapa */
-	@Prop() viewZoomMap: number = 8;
+	@Prop() viewZoomMap: number = 2;
 
 	/* Posicion en el globo terraqueo inicial ejemplo: [-4, 40] */
 	@Prop() viewPositionMap: number[] = [-4, 40];
@@ -40,13 +40,15 @@ export class CDKMap {
 		loadModules(
 			[
 				"esri/Map",
-				"esri/views/MapView"
+				"esri/views/MapView",
+				"esri/layers/FeatureLayer"
 			],
 			this.esriMapOptions
 		).then(
-			([Map, MapView]: [
+			([Map, MapView, FeatureLayer]: [
 				__esri.MapConstructor,
-				__esri.MapViewConstructor
+				__esri.MapViewConstructor,
+				any
 			]) => {
 				const mapDiv = this.rippleLinkElement;
 
@@ -60,6 +62,67 @@ export class CDKMap {
 					center: this.viewPositionMap,
 					map: this.esriMap
 				});
+
+				// Trailheads feature layer (points)
+				const trailheadsLayer = new FeatureLayer({
+					url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
+				});
+
+				// trailheadsLayer.setFeatureReduction({
+				// 	type: "cluster",
+				// 	clusterRadius: 60
+				// });
+
+				this.esriMap.add(trailheadsLayer);
+
+				// symbol
+				// const trailheadsRenderer = {
+				// 	type: "simple",
+				// 	symbol: {
+				// 		type: "picture-marker",
+				// 		url: "http://static.arcgis.com/images/Symbols/NPS/npsPictograph_0231b.png",
+				// 		width: "18px",
+				// 		height: "18px"
+				// 	}
+				// }
+
+				// const trailheadsLabels = {
+				// 	symbol: {
+				// 		type: "text",
+				// 		color: "#FFFFFF",
+				// 		haloColor: "#5E8D74",
+				// 		haloSize: "2px",
+				// 		font: {
+				// 			size: "12px",
+				// 			family: "Noto Sans",
+				// 			style: "italic",
+				// 			weight: "normal"
+				// 		}
+				// 	},
+				// 	labelPlacement: "above-center",
+				// 	labelExpressionInfo: {
+				// 		expression: "$feature.TRL_NAME"
+				// 	}
+				// };
+
+				// const trailheads = new FeatureLayer({
+				// 	url:
+				// 		"https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0",
+				// 	renderer: trailheadsRenderer,
+				// 	labelingInfo: [trailheadsLabels],
+				// 	popupTemplate: {
+				// 		title: "Hola Mundo",
+				// 		content: "Esto es una prueba de texto de un popup"
+				// 	}
+				// });
+
+				// trailheads.setFeatureReduction({
+				// 	type: "cluster",
+				// 	clusterRadius: 60
+				//   });
+
+				// this.esriMap.add(trailheads);
+
 			}
 		);
 	}
